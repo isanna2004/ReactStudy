f389ee759d9d75fbca2c7b1b90665351fd3da2cd
Isanna <isanna19276@gmail.com>
2020-07-16 17:16:49 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Wrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: 1500,
+      time: null,
+      minutes:null,
+  
+    };
+  }
+  render() {
+    const StartTimer = () => {
+      let minutes = Math.floor(this.state.seconds / 60),
+        remSeconds = this.state.seconds % 60;
+       
+ if (remSeconds === 0) {
+        minutes = minutes - 1;
+        remSeconds = 60;
+      } ;
+      let timer = setInterval(() => {
+        remSeconds = remSeconds - 1;       
+      }, 1000)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+      return (
+        <h2>
+          Осталось {minutes} минут {remSeconds} секунд
+        </h2>
+      );
+}        
+    
+
+
+    const Button = () => {
+      return (
+        <div>
+          <button className="btn-lg mr-5" onClick={StartTimer}>
+            Start
+          </button>
+          <button className="btn-lg mr-5">Pause </button>
+          <button className="btn-lg">Stop </button>
+        </div>
+      );
+    };
+
+    return (
+      <div className="my-5 text-center">
+        <h1 className="my-5">Time to do your job</h1>
+        <Button />
+        <StartTimer />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Wrapper />, document.getElementById("root"));
+
 serviceWorker.unregister();
