862348120fe57c1f4a8e946c2717b37b2e4a7b14
Isanna <isanna19276@gmail.com>
2020-07-18 02:48:12 +0600

    second commit

--- src/index.js
+++ src/index.js
@@ -3,54 +3,58 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
 class Wrapper extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      seconds: 1500,
-      time: null,
-      minutes:null,
-  
+      seconds: 10,
+      timer: false,
     };
+    this.StartTime = this.StartTime.bind(this);
+    this.Timer = this.Timer.bind(this);
   }
+  StartTime() {
+    let minutes = Math.floor(this.state.seconds / 60);
+    let remSeconds = this.state.seconds % 60;
+    if (remSeconds === -1 && minutes >= 0) {
+      minutes = minutes - 1;
+      remSeconds = 59;
+    }
+if(minutes < 0) {
+  clearInterval(this.state.timer)
+}
+    this.setState((prevState, props) => {
+      return {
+        minutes: minutes,
+        remSeconds: remSeconds,git
+        seconds: prevState.seconds - 1,
+      };
+    });
+  }
+  Timer() {
+    let timer = setInterval(this.StartTime, 1000);
+    this.setState({ timer: timer });
+    return timer;
+  }
+
+  RestTimer() {
+    this.setState({ seconds: this.state.seconds / 5 });
+    return this.Timer();
+  }
+
   render() {
-    const StartTimer = () => {
-      let minutes = Math.floor(this.state.seconds / 60),
-        remSeconds = this.state.seconds % 60;
-       
- if (remSeconds === 0) {
-        minutes = minutes - 1;
-        remSeconds = 60;
-      } ;
-      let timer = setInterval(() => {
-        remSeconds = remSeconds - 1;       
-      }, 1000)
-
-      return (
-        <h2>
-          Осталось {minutes} минут {remSeconds} секунд
-        </h2>
-      );
-}        
-    
-
-
-    const Button = () => {
-      return (
-        <div>
-          <button className="btn-lg mr-5" onClick={StartTimer}>
-            Start
-          </button>
-          <button className="btn-lg mr-5">Pause </button>
-          <button className="btn-lg">Stop </button>
-        </div>
-      );
-    };
-
     return (
       <div className="my-5 text-center">
-        <h1 className="my-5">Time to do your job</h1>
-        <Button />
-        <StartTimer />
+        <h1>
+          Осталось {this.state.minutes} минут {this.state.remSeconds} секунд
+        </h1>
+
+        <button className="btn-lg mr-5" onClick={() => this.Timer()}>
+          Start
+        </button>
+        <button className="btn-lg mr-5" onClick={() => this.RestTimer()}>
+          Rest{" "}
+        </button>
+        <button className="btn-lg">Stop </button>
       </div>
     );
   }
