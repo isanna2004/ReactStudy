1dea83e1cc9462b333b557b776fdb983a2281f0d
Isanna <isanna19276@gmail.com>
2020-07-19 14:32:52 +0600

    added Pause

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
+import "../src/index.css";
 
 class Wrapper extends React.Component {
   constructor() {
     super();
     this.state = {
-      seconds: 70,
-      timer: false,
+      seconds: 10,
     };
     this.StartTime = this.StartTime.bind(this);
     this.Timer = this.Timer.bind(this);
+    this.RestTimer = this.RestTimer.bind(this);
+    this.PauseTimer = this.PauseTimer.bind(this);
+    this.StopTimer = this.StopTimer.bind(this);
   }
   StartTime() {
     let minutes = Math.floor(this.state.seconds / 60);
@@ -19,9 +22,12 @@ class Wrapper extends React.Component {
       minutes = minutes - 1;
       remSeconds = 59;
     }
-if(minutes < 0) {
-  clearInterval(this.state.timer)
-}
+    if (minutes < 0) {
+      clearInterval(this.state.timer);
+      minutes = 0;
+      remSeconds = 0;
+      return this.RestTimer();
+    }
     this.setState((prevState, props) => {
       return {
         minutes: minutes,
@@ -30,6 +36,7 @@ if(minutes < 0) {
       };
     });
   }
+
   Timer() {
     let timer = setInterval(this.StartTime, 1000);
     this.setState({ timer: timer });
@@ -40,21 +47,33 @@ if(minutes < 0) {
     this.setState({ seconds: this.state.seconds / 5 });
     return this.Timer();
   }
+  PauseTimer() {
+    clearInterval(this.state.timer);
+  }
+  StopTimer() {
+    clearInterval(this.state.timer);
+
+    console.log(this.state.timer);
+  }
 
   render() {
     return (
       <div className="my-5 text-center">
-        <h1>
+        <h1 className="title">Pomodoro Timer</h1>
+        <h2>
           Осталось {this.state.minutes} минут {this.state.remSeconds} секунд
-        </h1>
-
-        <button className="btn-lg mr-5" onClick={() => this.Timer()}>
-          Start
-        </button>
-        <button className="btn-lg mr-5" onClick={() => this.RestTimer()}>
-          Rest{" "}
-        </button>
-        <button className="btn-lg">Stop </button>
+        </h2>
+        <div className="button-group">
+          <button className="btn-lg mr-5" onClick={this.Timer}>
+            Start
+          </button>
+          <button className="btn-lg mr-5" onClick={this.PauseTimer}>
+            Pause
+          </button>
+          <button className="btn-lg" onClick={this.StopTimer}>
+            Stop
+          </button>
+        </div>
       </div>
     );
   }
